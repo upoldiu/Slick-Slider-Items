bc5eaa5a8b55363ef4b40e7b856f2727eb2b0408
Mahfujur Rahman Upol <83024340+upoldiu@users.noreply.github.com>
2021-05-04 02:22:56 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('.service_slide').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        speed: 1000,
+    });
+});
+
